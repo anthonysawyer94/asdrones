c29f59cd447f995cd41410d4698e34a685359bf4
Anthony <ajsawyer94@gmail.com>
2026-06-11 15:23:26 -0700

    include fora in urls

--- asdrones/urls.py
+++ asdrones/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('main.urls')),
+    path('fora/', include('fora.urls')),
 ]
 
 if not settings.PRODUCTION:
